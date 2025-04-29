288603de561b282109e3dfe60e1f654c95c3c3d7
Technosoph <119736053+Felix-at-Swarm@users.noreply.github.com>
2025-04-29 11:03:27 +0200

    update testimonials

---
+++ components/header.js
@@ -0,0 +1,29 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Buidler Fest #2</title>
+        <link href="/static/styles.css" rel="stylesheet" type="text/css" />
+        <link rel="icon" href="/static/favicon-32x32-dark.ico" type="image/x-icon" />
+      </head>
+      <header>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/agenda">Agenda</a>
+          <a href="/posts">Posts</a>
+          <a href="/gallery">Gallery</a>
+          <a href="/testimonials">Testimonials</a>
+          <a href="/register">Register</a>
+        </nav>
+      </header>
+    `;
+  }
+}
+
+customElements.define("header-component", Header);
